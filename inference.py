13e275c1140f2b1d917279b67ea87a3ab1288859
Vincent-luo <1486891983@qq.com>
2023-06-27 03:51:12 +0000

    Improve script

--- inference.py
+++ inference.py
@@ -1,6 +1,34 @@
 from dinov2.models.vision_transformer import vit_base
+import torch
+from torchvision import transforms
+from PIL import Image
+import requests
 
+# load model
 model = vit_base()
+# equip the model with the pre-trained weights
+state_dict = torch.hub.load_state_dict_from_url('https://dl.fbaipublicfiles.com/dinov2/dinov2_vitb14/dinov2_vitb14_pretrain.pth',
+                                                map_location="cpu")
 
-for name,param in model.named_parameters():
-    print(name, param.shape)
+model.load_state_dict(state_dict)
+
+# load image
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+# preprocess image
+transformations = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],  # these are RGB mean+std values
+        std=[0.229, 0.224, 0.225]  # across a large photo dataset
+    )
+])
+
+pixel_values = transformations(image).unsqueeze(0)
+
+outputs = model.forward_features(pixel_values)
+
+print("Outputs:", outputs)
